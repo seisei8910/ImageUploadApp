c7c703af94e081cb4d461952f298017c9d2f0914
SeiseiUtaka <185202718+seisei8910@users.noreply.github.com>
2025-03-09 01:13:50 +0000

    [Add] imagesコントローラーとViewを作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get 'images/index'
+  get 'images/new'
+  get 'images/edit'
+  get 'images/show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
