4917dbc7dd6d42c424fdd5a5ae4f939ddd50bcd7
SeiseiUtaka <185202718+seisei8910@users.noreply.github.com>
2025-03-09 01:15:46 +0000

    [Add] routesファイルを編集

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
-  get 'images/index'
-  get 'images/new'
-  get 'images/edit'
-  get 'images/show'
+  resources :images
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
